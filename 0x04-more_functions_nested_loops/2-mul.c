c677e5bb80bab390cbafcf7f40ea4cd9d1e602c5
Adeola <adeolaxdeus@gmail.com>
2024-04-27 11:54:50 +0100

    simple multiplication code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: The result of the two operand
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
