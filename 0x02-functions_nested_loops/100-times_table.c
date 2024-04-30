a85298d80090fb29d3512936e973fd800ef63ada
Adeola <adeolaxdeus@gmail.com>
2024-04-30 17:59:41 +0100

    100-times_table.c file updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void print_number(int num);
 /**
  * print_times_table - prints the n times table, starting with 0.
  * @n: the times table to print
@@ -16,27 +17,7 @@ void print_times_table(int n)
 			while (col <= n)
 			{
 				result = col * row;
-				if (result <= 9)
-				{
-					if (col != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(result + 0x30);
-				}
-				else if (result > 9 && result < 100)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + 0x30);
-					_putchar((result % 10) + 0x30);
-				}
-				else
-				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
+				print_number(result);
 				if (col != n)
 				{
 					_putchar(',');
@@ -50,5 +31,30 @@ void print_times_table(int n)
 			}
 			row++;
 		}
+	}
+}
+/**
+ * print_number - prints a number with proper alignment.
+ * @num: the number to print
+ */
+void print_number(int num)
+{
+	if (num < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(num + '0');
+	}
+	else if (num < 100)
+	{
+		_putchar(' ');
+		_putchar(num / 10 + '0');
+		_putchar(num % 10 + '0');
+	}
+	else
+	{
+		_putchar(num / 100 + '0');
+		_putchar((num / 10) % 10 + '0');
+		_putchar(num % 10 + '0');
 	}
 }
