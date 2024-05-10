fecaf7f587e07f14c820607f34baffc53c559f25
Adeola <adeolaxdeus@gmail.com>
2024-05-10 22:35:41 +0100

    caps by miles

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: pointer to a string
+ *
+ * Return: pointer to str
+ */
+char *cap_string(char *str)
+{
+	char *c = str;
+
+	while (*c)
+	{
+		if (*c == ',' || *c == ';' || *c == '.' || *c == '!'
+			|| *c == '?' || *c == ':' || *c == '(' || *c == ')'
+			|| *c == '{' || *c == '}' || *c == ' ' || *c == '\t'
+			|| *c == '\n')
+		{
+			if (*(c + 1) >= 'a' && *(c + 1) <= 'z')
+			{
+				*(c + 1) = ((*(c + 1) - 'a') + 'A');
+			}
+		}
+		c++;
+	}
+	return (str);
+}
