4af0dda47d28c15bfbd15c574ab4b99833d34b64
Adeola <adeolaxdeus@gmail.com>
2024-05-11 20:09:50 +0100

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+/**
+ * _strchr - locates a character in a string.
+ * @s: pointer to a memory area
+ * @c: character to be searched for
+ *
+ * Return:  a pointer to the first occurrence of the character c,
+ * in the string s, or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
