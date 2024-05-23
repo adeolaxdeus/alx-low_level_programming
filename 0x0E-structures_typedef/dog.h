cac9f4b2f32068a3a6fbf5584fceea73b3bffbe2
Adeola <adeolaxdeus@gmail.com>
2024-05-23 07:21:43 +0100

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define a new type with the following elements
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
