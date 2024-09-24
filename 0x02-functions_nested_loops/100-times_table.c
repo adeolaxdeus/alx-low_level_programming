09cfcbda17768eaf7914c2a220f20f96d86219f2
adeolaxdeus <adeolaxdeus@gmail.com>
2024-09-24 04:02:19 -0700

    lets fucking go

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,63 @@
 #include "main.h"
-void print_number(int num);
 /**
- * print_times_table - prints the n times table, starting with 0.
- * @n: the times table to print
+ * print - Print numbers
+ * @x: Required . Number to be print.
+ * @flag: Required. Determine when to print space before a digit.
+ *
+ */
+void print(int x, int flag)
+{
+	if (x > 99)
+	{
+		_putchar((x / 100) + '0');
+		_putchar(((x / 10) % 10) + '0');
+		_putchar((x % 10) + '0');
+	}
+	else if (x > 9 && x < 100)
+	{
+		_putchar(' ');
+		_putchar((x / 10) + '0');
+		_putchar((x % 10) + '0');
+	}
+	else
+	{
+		if (flag != 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar(x + '0');
+	}
+}
+
+/**
+ * print_times_table - Print n times table starting with 0.
+ * @n: Required. Number of times table.
  *
  */
 void print_times_table(int n)
 {
-	int row = 0, col, result;
+	int col = 0;
+	int row, flag;
 
-	if (n <= 15 && n > 0)
+	if (n < 16 && n >= 0)
 	{
-		while (row <= n)
+		while (col <= n)
 		{
-			col = 0;
-			while (col <= n)
+			row = 0;
+			while (row <= n)
 			{
-				result = col * row;
-				print_number(result);
-				if (col != n)
+				flag = row;
+				print(col * row, flag);
+				if (row != 0 && row != n)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (col == n)
-				{
-					_putchar('\n');
-				}
-				col++;
+				row++;
 			}
-			row++;
+			_putchar('\n');
+			col++;
 		}
-	}
-}
-/**
- * print_number - prints a number with proper alignment.
- * @num: the number to print
- */
-void print_number(int num)
-{
-	if (num < 10)
-	{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(num + '0');
-	}
-	else if (num < 100)
-	{
-		_putchar(' ');
-		_putchar(num / 10 + '0');
-		_putchar(num % 10 + '0');
-	}
-	else
-	{
-		_putchar(num / 100 + '0');
-		_putchar((num / 10) % 10 + '0');
-		_putchar(num % 10 + '0');
 	}
 }
