d37bb342b749eb54e4eb92260e5e71baa2964538
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-10 23:43:50 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
 /**
- * _strncpy -  copies a string.
- * @dest: pointer to a buffer
- * @src: pointer to string to be copied to dest
- * @n: number of bytes to be  copied.
+ * _strncpy - Copies a string.
+ * @dest: Pointer to a destination string
+ * @src: Pointer to string to be copied.
+ * @n: Number of bytes to copy from src to dest.
  *
- * Return: pointer to dest
+ * Description: If there is no null byte among first n bytes of src,
+ * the string placed in dest will not be null terminated.
+ *
+ * Return: pointer to dest.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *temp = dest;
-	int index = 0;
+	char *ptr = dest;
+	int i = 0;
 
-	while (index < n && src[index] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
-		temp[index] = src[index];
-		index++;
+		ptr[i] = src[i];
+		i++;
 	}
-	while (index < n)
-	{
-		temp[index] = '\0';
-		index++;
-	}
-	return (dest);
+	if (src[i] == '\0')
+		ptr[i] = '\0';
+	return (ptr);
 }
