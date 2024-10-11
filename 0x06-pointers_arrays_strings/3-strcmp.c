0b011bc1c1eabb6fb3d67727b79830ff60e65889
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-11 08:00:15 -0700

    implement a subtraction logic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,8 @@
  * @s1: Pointer to first string.
  * @s2: Pointer to second string to be compare to previous.
  *
- * Return: 0 if both string are same or 1 if if first s1 is > s2
- * lexicographically, -1 otherwise.
+ * Return: 0 if both string are same otherwise difference btw s1
+ *  and s2 lexicographically.
  *
  */
 int _strcmp(char *s1, char *s2)
@@ -16,18 +16,10 @@ int _strcmp(char *s1, char *s2)
 			s1++;
 			s2++;
 		}
-		else if (*s1 > *s2)
-		{
-			return (1);
-		}
 		else
 		{
-			return (-1);
+			return (*s1 - *s2);
 		}
 	}
-	if (*s1)
-		return (1);
-	else if (*s2)
-		return (-1);
 	return (0);
 }
