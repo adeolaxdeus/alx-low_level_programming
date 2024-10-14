3a595bc4094e3c9c11ee861aedbdc34628c3ebff
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-14 00:01:07 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,54 +1,24 @@
 #include "main.h"
-void print(int num);
 /**
- * print_number - Entry point
- * @n: integer to be print
+ * print_number - Prints an integer with putchar
+ * @n: Required. A number.
  *
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
-		n *= -1;
+		x = (unsigned int)n * -1;
 		_putchar('-');
-		print(n);
-	}
-	else if (n == 0)
-	{
-		_putchar(n + 0x30);
-	}
-	else if (n > 0)
-	{
-		print(n);
-	}
-}
-/**
- * print - print number values
- * @num: digit to be print
- *
- */
-void print(int num)
-{
-	if (num < 10)
-	{
-		_putchar(num + 0x30);
-	}
-	else if (num > 9 && num < 100)
-	{
-		_putchar((num / 10) + 0x30);
-		_putchar((num % 10) + 0x30);
-	}
-	else if (num > 99 && num < 1000)
-	{
-		_putchar((num / 100) + 0x30);
-		_putchar(((num / 10) % 10) + 0x30);
-		_putchar((num % 10) + 0x30);
 	}
 	else
 	{
-		_putchar((num / 1000) + 0x30);
-		_putchar(((num / 100) % 10) + 0x30);
-		_putchar(((num / 10) % 10) + 0x30);
-		_putchar((num % 10) + 0x30);
+		x = (unsigned int)n;
 	}
+
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
