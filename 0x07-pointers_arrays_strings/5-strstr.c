bbfc17460687a19dd48e0c4dbbe390e86950e46e
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-16 01:00:34 -0700

    Just match it

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,32 @@
 /**
+ * _match - Find a word match in a string
+ * word: Word to be searched for in string
+ * str: String to be scan.
+ *
+ * Return: 1 if word match is found otherwise 0
+ */
+int _match(char *word, char *str)
+{
+	char *ptr = word;
+	int i = 0, x = 0;
+
+	while (*ptr) /* get length of word */
+	{
+		i++;
+		ptr++;
+	}
+	while (*word && *str)
+	{
+		if (*word == *str)
+			x++;
+		word++;
+		str++;
+	}
+	if (x == i)
+		return (1);
+	return (0);
+}
+/**
  * _strstr - Locates a substring
  * @haystack: Pointer to be scan for substring needdle.
  * @needle: Pointer to substring to be located
@@ -16,7 +44,10 @@ char *_strstr(char *haystack, char *needle)
 		while (*ptr)
 		{
 			if (*ptr == *needle)
-				return (ptr);
+			{
+				if (_match(needle, ptr))
+					return (ptr);
+			}
 			ptr++;
 		}
 		needle++;
