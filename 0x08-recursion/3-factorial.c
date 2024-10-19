27470fa0570aa542510a3798f40efae5def56402
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-18 17:34:53 -0700

    It ain't bad to dream bigger darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - Returns the factorial of a given number
+ * @n: A number
+ *
+ * Return: Factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
