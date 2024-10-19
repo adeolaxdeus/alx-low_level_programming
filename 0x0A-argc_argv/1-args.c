1bc7aa6231b2a35362195f23688c56f7b52cc2f9
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-18 23:26:17 -0700

    silence is argument carried by another means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - prints the number of arguments passed into it.
- * @argc: number of command line argument passed to program
- * @argv: argument passed  to function
+ * main - Prints the number of arguments passed to it
+ * @argc: Number of argument passed
+ * @argv: Pointer to array of args passed
  *
- * Return: Always 0
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	(void) argv;
 
 	printf("%d\n", argc - 1);
 	return (0);
