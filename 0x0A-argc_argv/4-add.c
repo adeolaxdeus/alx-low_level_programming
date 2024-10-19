b5596bc891eac1fd4e92806dbeab6e76ae75bf7b
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-19 04:25:55 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,53 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _isdigit - check if a character is number or not
- * @c: value to be checked
+ * main - Adds positive number
+ * @argc: Number of arguments passed
+ * @argv: Pointer to array of numbers passed
  *
- * Return: 1 on success or 0 otherwise
- */
-int _isdigit(char c)
-{
-	if (c >= '0' && c <= '9')
-	{
-		return (1);
-	}
-	return (0);
-}
-/**
- * main - a program that adds positive numbers.
- * @argc: number of digit  passed to the program
- * @argv: digit to be multiply
- *
- * Return: 0 on success, or 1 otherwise.
+ * Return: 0 on success otherwise 1
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0, num, i = 1, j;
-	char *arg;
+	int total, i = 1;
 
 	if (argc == 1)
 	{
-		printf("0\n");
+		puts("0");
 		return (0);
 	}
 	while (i < argc)
 	{
-		arg = argv[i];
-		j = 0;
-		while (arg[j] != '\0')
+		if (atoi(argv[i]))
 		{
-			if (!_isdigit(arg[j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
+			total += atoi(argv[i]);
 		}
-		num = atoi(arg);
-		sum += num;
+		else
+		{
+			puts("Error");
+			return (1);
+		}
 		i++;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", total);
 	return (0);
 }
