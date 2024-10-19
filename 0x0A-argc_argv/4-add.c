fef3bcbb33d72f08e55d2fea24a5287a99bdbcfe
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-19 06:19:57 -0700

    fix: if a non-dihit is passed btw digit eg 12e45

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * check - If a string consist of integers only
+ * @ptr: Pointer to string to be check
+ *
+ * Return: 1 if ptr consist of digits only otherwise 0.
+ */
+int check(char *ptr)
+{
+	char *temp = ptr;
+
+	while (*temp)
+	{
+		if (*temp >= '0' && *temp <= '9')
+			temp++;
+		else
+			return (0);
+	}
+	return (1);
+}
+/**
  * main - Adds positive number
  * @argc: Number of arguments passed
  * @argv: Pointer to array of numbers passed
@@ -18,7 +37,7 @@ int main(int argc, char *argv[])
 	}
 	while (i < argc)
 	{
-		if (atoi(argv[i]))
+		if (check(argv[i]))
 		{
 			total += atoi(argv[i]);
 		}
