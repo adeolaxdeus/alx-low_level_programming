f41e1794eda8fbf33a3fc5ae730425274d83e682
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-21 03:05:22 -0700

    fix: How to handle If NULL pointer is passed as args

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *ptr, *temp;
 	unsigned int size;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	size = strlen(s1);
 	size += strlen(s2);
 	ptr = malloc(sizeof(char) * (size + 1));
