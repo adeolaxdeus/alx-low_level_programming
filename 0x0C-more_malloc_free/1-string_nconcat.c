9770ba5d013d30c7b5210e87d122715eff18c57b
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-23 15:43:45 -0700

    string it

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,64 +1,41 @@
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * checkForNull - check if a pointer to a memory is null
- * @str1: pointer to memory 1
- * @str2: pointer to memory 2
- */
-void checkForNull(char *str1, char *str2)
-{
-	if (str1 == NULL)
-	{
-		*str1 = ' ';
-	}
-	if (str2 == NULL)
-	{
-		*str2 = ' ';
-	}
-}
-/**
- * string_nconcat - concatenates two strings.
- * @s1: first string
- * @s2: second string
- * @n: number of bytes to be copy from s2.
- *
- * Return: pointer to a newly allocated space in memory which contains s1
- * folloed by first n bytes of s2 or NULL if funtion fails
- *
+ * string_nconcat - Concatenates two strings.
+ * @s1: Pointer to first string
+ * @s2: Pointer to second string
+ * @n: Number of bytes from s2 to concatenate to s1
+ * Return: Pointer To newly allocated memory containing s1 followed by
+ * n bytes of s2 on success otherwise NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, index1 = 0, index2 = 0;
-	char *ptr;
+	char *ptr, *temp;
+	int i;
 
-	checkForNull(s1, s2);
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	if (n >= len2)
-	{
-		n = len2;
-	}
-	ptr = malloc((len1 + n) * sizeof(char));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = strlen(s1);
+	i += n;
+	ptr = malloc(sizeof(char) * i + 1);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	while (index1 < len1)
+	temp = ptr;
+	while (*s1)
 	{
-		ptr[index1] = s1[index1];
-		index1++;
+		*ptr = *s1;
+		ptr++;
+		s1++;
 	}
-	while (index2 < n)
+	while (*s2 && n > 0)
 	{
-		ptr[index1] = s2[index2];
-		index1++;
-		index2++;
+		*ptr = *s2;
+		ptr++;
+		s2++;
+		n--;
 	}
-	return (ptr);
+	*ptr = '\0';
+	return (temp);
 }
