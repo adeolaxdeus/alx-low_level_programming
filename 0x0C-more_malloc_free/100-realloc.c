4d2b67136e3338be72ff6a0ff9d2357b31aff50d
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-25 09:09:03 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr != NULL && new_size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
