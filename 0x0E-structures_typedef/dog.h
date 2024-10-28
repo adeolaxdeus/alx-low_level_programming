05f3728c3b0f1211f8df9e75aa6b2ec553f3ae13
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-27 19:30:26 -0700

    You want loyalty, buy a husky

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,17 @@
 #define DOG_H
 
 /**
- * struct dog - define a new type with the following elements
- * @name: name of dog
- * @age: age of dog
- * @owner: name of dog owner
+ * dog - Defines a dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-void free_dog(dog_t *d);
+} dog;
 
 #endif
