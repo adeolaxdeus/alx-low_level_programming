5101ab08b7695b10d51190d615fbbb4502df7f0a
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-28 04:51:37 -0700

    free it if malloc fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,12 +18,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	dog_ptr->name = malloc(sizeof(name) + 1);
 	if (dog_ptr->name == NULL)
+	{
+		free(dog_ptr->name);
+		dog->name = NULL;
+		free(dog_ptr);
+		dog_ptr = NULL;
 		return (NULL);
+	}
 	strcpy(dog_ptr->name, name);
 	dog_ptr->age = age;
 	dog_ptr->owner = malloc(sizeof(owner) + 1);
 	if (dog_ptr->owner == NULL)
+	{
+		free(dog_ptr->owner);
+		dog_ptr->owner = NULL;
+		free(dog_ptr);
+		dog_ptr = NULL;
 		return (NULL);
+	}
 	strcpy(dog_ptr->owner, owner);
 	return (dog_ptr);
 }
