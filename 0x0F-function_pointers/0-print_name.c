c5c017446d16bff83b0e6a188cfac9c00b9e6fda
adeolaxdeus <adeolaxdeus@gmail.com>
2024-10-30 04:28:10 -0700

    Check if pointer passed is null or not before using it

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - prints a name
  * @name: Pointer to name
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		return;
 	f(name);
 }
