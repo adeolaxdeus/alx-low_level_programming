6bcba5212923d615b26fdb471023aa2c573af9f7
adeolaxdeus <adeolaxdeus@gmail.com>
2024-12-17 12:07:19 -0800

    allkcate memory for the arrays in the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,10 +9,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i = 0;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		hash_table->array[i] = NULL;
+		i++;
+	}
 	return (hash_table);
 }
