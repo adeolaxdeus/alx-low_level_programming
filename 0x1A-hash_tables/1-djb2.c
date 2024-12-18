dc5e8e4e4db6517929c2035f83d3346e192cbf1c
adeolaxdeus <adeolaxdeus@gmail.com>
2024-12-18 01:02:19 -0800

    djb2 algo

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,17 @@
+/**
+ * hash_djb2 -  A hash function that implement the djb2 algo
+ * @str:  Pointer to string to be passed to the hash function
+ *
+ * Return: Hashed number
+ */
+unsigned long int hash_djb2(unsigned const char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 32 + c */
+	}
+	return (hash);
+}
