2f200b0fab4dd7feaf20d300a7cde73e16a10433
adeolaxdeus <adeolaxdeus@gmail.com>
2024-12-18 03:08:43 -0800

    init hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,6 +9,7 @@ unsigned long int hash_djb2(unsigned const char *str)
 	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 32 + c */
